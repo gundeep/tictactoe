aa2aec5481377abb0ef0b9f0eaab7f83ee3cf508
gundeepsingh <gundeep.upenn@gmail.com>
2022-04-07 17:56:46 +0200

    fixed the board, UI messed still

--- src/app/booard/booard.component.ts
+++ src/app/booard/booard.component.ts
@@ -8,27 +8,59 @@ import { Component, OnInit } from '@angular/core';
 export class BooardComponent implements OnInit {
   squares: any[];
   XIsNext: boolean;
-  winner: string;
+  winner: string | null;
 
   constructor() { 
-    this.squares = Array(9).fill(null);
-    this.winner = "none";
-    this.XIsNext = true;
+  }
+
+  ngOnInit(){
+    this.newGame();
   }
 
   newGame() { 
     this.squares = Array(9).fill(null);
-    this.winner = "none";
+    this.winner = null ;
     this.XIsNext = true;
   }
 
   get player()
   {
-    return this.XIsNext ? 'X' : '0'
+    return this.XIsNext ? 'X' : '0';
   }
 
-  ngOnInit(): void {
-    this.newGame();
+  makemove(idx: number)
+  {
+    if (!this.squares[idx])
+    {
+      this.squares.splice(idx, 1, this.player);
+      this.XIsNext = !this.XIsNext;
+    }
+    this.winner = this.calculateWinner();
+  }
+
+  calculateWinner()
+  {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
   }
 
 }
